4eda5f117419ddba5449f8d7d258a0bcbd9db3ab
Kuldeep Singh <in.singhkuldeep@gmail.com>
2021-06-24 10:15:41 +0100

    App insights added

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace ResumeWebsite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

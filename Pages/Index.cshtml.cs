7162b82d569a02c8c32a3a559f635bf0ced9167b
Kuldeep Singh <in.singhkuldeep@gmail.com>
2021-07-04 03:12:36 +0100

    certificate list moved to .cs file

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,6 +11,7 @@ namespace ResumeWebsite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public IList<string> Badges { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +20,25 @@ namespace ResumeWebsite.Pages
 
         public void OnGet()
         {
-
+            Badges = new List<string>
+            {
+                "images/01-AzSolutionArchitect-badge.png",
+                "images/02-DevOpsExpert-badge.png",
+                "images/03-AzDataEngineer-badge.png",
+                "images/04-DataAnalystAssociate-badge.png",
+                "images/04-DatabaseAdministrator-badge.png",
+                "images/azureAdministrator600.png",
+                "images/azuredeveloperassociate600.png",
+                "images/adf.png",
+                "images/cosmosL200.jpg",
+                "images/accessibility.png",
+                //"images/xamarin.png",
+                "images/01-exam-az300.png",
+                "images/01-exam-az301.png",
+                "images/02-exam-az400.png",
+                "images/03-exam-dp200.png",
+                "images/03-exam-dp201.png"
+            };
         }
     }
 }
